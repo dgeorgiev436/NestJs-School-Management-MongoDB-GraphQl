d1b309c301b2168f15720bcb07a1a1312514741e
dgeorgiev436 <68106371+dgeorgiev436@users.noreply.github.com>
2022-07-18 13:40:57 +0100

    Creating a lesson module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 
 
 @Module({
-  imports: []
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true // In the code first approach, you use decorators and TypeScript classes to generate the corresponding GraphQL schema.
+    })
+  ]
 })
 export class AppModule {}
