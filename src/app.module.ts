af2c690547f0249470865985936a62c48cf311e8
dgeorgiev436 <68106371+dgeorgiev436@users.noreply.github.com>
2022-07-18 13:47:42 +0100

    Creating a Lesson schema type

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { LessonModule } from './lesson/lesson.module';
 
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: true // In the code first approach, you use decorators and TypeScript classes to generate the corresponding GraphQL schema.
-    })
+    }),
+    LessonModule
   ]
 })
 export class AppModule {}
---
+++ src/lesson/lesson.type.ts
@@ -0,0 +1,18 @@
+import { Field, ID, ObjectType } from "@nestjs/graphql";
+
+
+@ObjectType("Lesson") // Decorate as a object type to tell GraphQL that this is a type. The argument in the decorator changes the name of the type from LessonType to Lesson
+export class LessonType {
+
+    @Field(type => ID) // First argument of the decorator tells graphql the type of the field. ID is a type that comes with GraphQl
+    id: string;
+
+    @Field() // The Field decorator tells GraphQL that this is a field
+    name: string;
+
+    @Field()
+    startDate: string;
+
+    @Field()
+    endDate: string;
+}
