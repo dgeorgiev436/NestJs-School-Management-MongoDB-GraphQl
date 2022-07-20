274610b385c485e3a52a5afc66632da9079c95c7
dgeorgiev436 <dgeorgiev436@gmail.com>
2022-07-20 01:20:33 +0000

    Assign students when lesson is created

--- src/lession/lession.service.ts
+++ src/lession/lession.service.ts
@@ -23,14 +23,14 @@ export class LessionService {
 
   async createLesson(createLessonInput: CreateLessonInput): Promise<Lesson> {
 	  
-	  const {name, startDate, endDate} = createLessonInput
+	  const {name, startDate, endDate, students} = createLessonInput
 
     const lesson = this.lessonRepository.create({
         id: uuid(),
         name,
         startDate,
         endDate,
-		students: []
+		students
     });
 
     return this.lessonRepository.save(lesson)
--- src/lession/lesson.input.ts
+++ src/lession/lesson.input.ts
@@ -1,5 +1,5 @@
-import { InputType, Field } from "@nestjs/graphql";
-import { MinLength, IsDateString } from "class-validator";
+import { InputType, Field, ID } from "@nestjs/graphql";
+import { MinLength, IsDateString, IsUUID } from "class-validator";
 
 @InputType()
 export class CreateLessonInput {
@@ -14,4 +14,8 @@ export class CreateLessonInput {
 	@IsDateString()
 	@Field()
 	endDate: string;
+	
+	@IsUUID("4", {each: true})
+	@Field(() => [ID], { defaultValue: [] } ) // Setting the default value as an empty array (When lession is initioally created without students assigned)
+	students: string[]
 }
